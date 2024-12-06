ef74f179444c6005a563ef90bcb56e280b06e7d9
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-06 15:06:41 +0600

    Create Fail.py

---
+++ Fail.py
@@ -0,0 +1,9 @@
+def fail(fail):
+        if  fail:
+            score=score1+score2+score3+score4+score5;
+            print(f"You fail.Your score is:{score}")
+        if fail and score==0:
+            fail==False
+             print(f"You Fail.Your score is:{score}")
+        
+    
